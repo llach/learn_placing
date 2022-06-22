03bfd1b54cd08cd8827b1f77fbdfa7f0a43c88fe
llach <lachluca@gmail.com>
2022-06-22 16:58:48 +0200

    stop fingers on contact

--- myrmex_gripper_controller/src/myrmex_gripper_controller/myrmex_gripper_controller.cpp
+++ myrmex_gripper_controller/src/myrmex_gripper_controller/myrmex_gripper_controller.cpp
@@ -147,104 +147,117 @@ void MyrmexGripperController::update(const ros::Time& time, const ros::Duration&
   // next control cycle, leaving the current cycle without a valid trajectory.
 
   // Update current state and state error
-  for (unsigned int i = 0; i < joints_.size(); ++i)
+  // TODO only when not doing FC!
+  if (rt_active_goal_)
   {
-    current_state_.position[i] = joints_[i].getPosition();
-    current_state_.velocity[i] = joints_[i].getVelocity();
-    // There's no acceleration data available in a joint handle
-
-    typename TrajectoryPerJoint::const_iterator segment_it = sample(curr_traj[i], time_data.uptime.toSec(), desired_joint_state_);
-    if (curr_traj[i].end() == segment_it)
+    for (unsigned int i = 0; i < joints_.size(); ++i)
     {
-      // Non-realtime safe, but should never happen under normal operation
-      ROS_ERROR_NAMED(name_,
-                      "Unexpected error: No trajectory defined at current time. Please contact the package maintainer.");
-      return;
-    }
-    desired_state_.position[i] = desired_joint_state_.position[0];
-    desired_state_.velocity[i] = desired_joint_state_.velocity[0];
-    desired_state_.acceleration[i] = desired_joint_state_.acceleration[0]; ;
+      current_state_.position[i] = joints_[i].getPosition();
+      current_state_.velocity[i] = joints_[i].getVelocity();
+      // There's no acceleration data available in a joint handle
 
-    state_joint_error_.position[0] = angles::shortest_angular_distance(current_state_.position[i],desired_joint_state_.position[0]);
-    state_joint_error_.velocity[0] = desired_joint_state_.velocity[0] - current_state_.velocity[i];
-    state_joint_error_.acceleration[0] = 0.0;
-
-    state_error_.position[i] = angles::shortest_angular_distance(current_state_.position[i],desired_joint_state_.position[0]);
-    state_error_.velocity[i] = desired_joint_state_.velocity[0] - current_state_.velocity[i];
-    state_error_.acceleration[i] = 0.0;
-
-    //Check tolerances
-    const RealtimeGoalHandlePtr rt_segment_goal = segment_it->getGoalHandle();
-    if (rt_segment_goal && rt_segment_goal == rt_active_goal_)
-    {
-      // Check tolerances
-      if (time_data.uptime.toSec() < segment_it->endTime())
+      typename TrajectoryPerJoint::const_iterator segment_it = sample(curr_traj[i], time_data.uptime.toSec(), desired_joint_state_);
+      if (curr_traj[i].end() == segment_it)
       {
-        // Currently executing a segment: check path tolerances
-        const joint_trajectory_controller::SegmentTolerancesPerJoint<Scalar>& joint_tolerances = segment_it->getTolerances();
-        if (!checkStateTolerancePerJoint(state_joint_error_, joint_tolerances.state_tolerance))
-        {
-          if (verbose_)
-          {
-            ROS_ERROR_STREAM_NAMED(name_,"Path tolerances failed for joint: " << joint_names_[i]);
-            checkStateTolerancePerJoint(state_joint_error_, joint_tolerances.state_tolerance, true);
-          }
-
-          if(rt_segment_goal && rt_segment_goal->preallocated_result_)
-          {
-            rt_segment_goal->preallocated_result_->error_code =
-            control_msgs::FollowJointTrajectoryResult::PATH_TOLERANCE_VIOLATED;
-            rt_segment_goal->setAborted(rt_segment_goal->preallocated_result_);
-            rt_active_goal_.reset();
-            successful_joint_traj_.reset();
-          }
-          else{
-            ROS_ERROR_STREAM("rt_segment_goal->preallocated_result_ NULL Pointer");
-          }
-        }
+        // Non-realtime safe, but should never happen under normal operation
+        ROS_ERROR_NAMED(name_,
+                        "Unexpected error: No trajectory defined at current time. Please contact the package maintainer.");
+        return;
       }
-      else if (segment_it == --curr_traj[i].end())
+
+      // don't move joint if it's in contact
+      if (sensor_states_[i] > NO_CONTACT) desired_joint_state_.position[0] = current_state_.position[i];
+
+      desired_state_.position[i] = desired_joint_state_.position[0];
+      desired_state_.velocity[i] = desired_joint_state_.velocity[0];
+      desired_state_.acceleration[i] = desired_joint_state_.acceleration[0]; ;
+
+      state_joint_error_.position[0] = angles::shortest_angular_distance(current_state_.position[i],desired_joint_state_.position[0]);
+      state_joint_error_.velocity[0] = desired_joint_state_.velocity[0] - current_state_.velocity[i];
+      state_joint_error_.acceleration[0] = 0.0;
+
+      state_error_.position[i] = angles::shortest_angular_distance(current_state_.position[i],desired_joint_state_.position[0]);
+      state_error_.velocity[i] = desired_joint_state_.velocity[0] - current_state_.velocity[i];
+      state_error_.acceleration[i] = 0.0;
+
+      //Check tolerances
+      const RealtimeGoalHandlePtr rt_segment_goal = segment_it->getGoalHandle();
+      if (rt_segment_goal && rt_segment_goal == rt_active_goal_)
       {
-        if (verbose_)
-          ROS_DEBUG_STREAM_THROTTLE_NAMED(1,name_,"Finished executing last segment, checking goal tolerances");
-
-        // Controller uptime
-        const ros::Time uptime = time_data_.readFromRT()->uptime;
-
-        // Checks that we have ended inside the goal tolerances
-        const joint_trajectory_controller::SegmentTolerancesPerJoint<Scalar>& tolerances = segment_it->getTolerances();
-        const bool inside_goal_tolerances = checkStateTolerancePerJoint(state_joint_error_, tolerances.goal_state_tolerance);
-
-        if (inside_goal_tolerances)
+        // Check tolerances
+        if (time_data.uptime.toSec() < segment_it->endTime())
         {
-          successful_joint_traj_[i] = 1;
+          // Currently executing a segment: check path tolerances
+          const joint_trajectory_controller::SegmentTolerancesPerJoint<Scalar>& joint_tolerances = segment_it->getTolerances();
+          if (!checkStateTolerancePerJoint(state_joint_error_, joint_tolerances.state_tolerance))
+          {
+            if (verbose_)
+            {
+              ROS_ERROR_STREAM_NAMED(name_,"Path tolerances failed for joint: " << joint_names_[i]);
+              checkStateTolerancePerJoint(state_joint_error_, joint_tolerances.state_tolerance, true);
+            }
+
+            if(rt_segment_goal && rt_segment_goal->preallocated_result_)
+            {
+              rt_segment_goal->preallocated_result_->error_code =
+              control_msgs::FollowJointTrajectoryResult::PATH_TOLERANCE_VIOLATED;
+              rt_segment_goal->setAborted(rt_segment_goal->preallocated_result_);
+              rt_active_goal_.reset();
+              successful_joint_traj_.reset();
+            }
+            else{
+              ROS_ERROR_STREAM("rt_segment_goal->preallocated_result_ NULL Pointer");
+            }
+          }
         }
-        else if (uptime.toSec() < segment_it->endTime() + tolerances.goal_time_tolerance)
-        {
-          // Still have some time left to meet the goal state tolerances
-        }
-        else
+        else if (segment_it == --curr_traj[i].end())
         {
           if (verbose_)
-          {
-            ROS_ERROR_STREAM_NAMED(name_,"Goal tolerances failed for joint: "<< joint_names_[i]);
-            // Check the tolerances one more time to output the errors that occurs
-            checkStateTolerancePerJoint(state_joint_error_, tolerances.goal_state_tolerance, true);
-          }
+            ROS_DEBUG_STREAM_THROTTLE_NAMED(1,name_,"Finished executing last segment, checking goal tolerances");
 
-          if(rt_segment_goal){
-            rt_segment_goal->preallocated_result_->error_code = control_msgs::FollowJointTrajectoryResult::GOAL_TOLERANCE_VIOLATED;
-            rt_segment_goal->setAborted(rt_segment_goal->preallocated_result_);
+          // Controller uptime
+          const ros::Time uptime = time_data_.readFromRT()->uptime;
+
+          // Checks that we have ended inside the goal tolerances
+          const joint_trajectory_controller::SegmentTolerancesPerJoint<Scalar>& tolerances = segment_it->getTolerances();
+          const bool inside_goal_tolerances = checkStateTolerancePerJoint(state_joint_error_, tolerances.goal_state_tolerance);
+
+          if (inside_goal_tolerances)
+          {
+            successful_joint_traj_[i] = 1;
+          }
+          else if (uptime.toSec() < segment_it->endTime() + tolerances.goal_time_tolerance)
+          {
+            // Still have some time left to meet the goal state tolerances
           }
           else
           {
-            ROS_ERROR_STREAM("rt_segment_goal->preallocated_result_ NULL Pointer");
+            if (verbose_)
+            {
+              ROS_ERROR_STREAM_NAMED(name_,"Goal tolerances failed for joint: "<< joint_names_[i]);
+              // Check the tolerances one more time to output the errors that occurs
+              checkStateTolerancePerJoint(state_joint_error_, tolerances.goal_state_tolerance, true);
+            }
+
+            if(rt_segment_goal){
+              rt_segment_goal->preallocated_result_->error_code = control_msgs::FollowJointTrajectoryResult::GOAL_TOLERANCE_VIOLATED;
+              rt_segment_goal->setAborted(rt_segment_goal->preallocated_result_);
+            }
+            else
+            {
+              ROS_ERROR_STREAM("rt_segment_goal->preallocated_result_ NULL Pointer");
+            }
+            rt_active_goal_.reset();
+            successful_joint_traj_.reset();
           }
-          rt_active_goal_.reset();
-          successful_joint_traj_.reset();
         }
       }
     }
+  }
+
+  if (state_ == FORCE_CTRL)
+  {
+
   }
 
   //If there is an active goal and all segments finished successfully then set goal as succeeded
