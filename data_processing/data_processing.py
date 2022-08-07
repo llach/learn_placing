3f4f820b8e674a9b5ed3023052d9ea9f50a63d1a
llach <lachluca@gmail.com>
2022-08-07 15:31:40 +0200

    read dataset

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+from yaml import load
 
 """
 TODO what is max, what is min value
@@ -21,38 +23,61 @@ def reshape_mm_vector(data):
     return np.reshape(data, (N,16,16))
 
 def normalize_mm(data):
-    """ converts values from range [0,4095] to [0,1]
+    """ converts values from range [0,4095] to [0,1], 1 == maximum force (hence 1-data)
     """
-    return data/4095
+    return 1-(data/4095)
 
+def preprocess_myrmex(data):
+    # (optional) since the outermost taxels are prone 
+    # to false positives, we can cut them away 
+    # data = remove_outer(data, B=1)
+    return normalize_mm(reshape_mm_vector(data))
+
+def sync_mm_sample(m1, m2):
+    """ TODO here we sync samples: right now it's some dummy function
+    """
+    return np.array([
+        m1[:40],
+        m2[:40]
+    ])
+
+def load_dataset(folder):
+    ds = {
+        "tactile": [],
+        "obj_vector": [],
+        "angle": [],
+        "ft": []
+    }
+    for fi in os.listdir(folder):
+        if "pkl" not in fi: continue
+        
+        pkl = f"{folder}/{fi}"
+        with open(pkl, "rb") as f:
+            sample = pickle.load(f)
+        
+        ds["tactile"].append(sync_mm_sample(
+            preprocess_myrmex(sample["myrmex_left"]),
+            preprocess_myrmex(sample["myrmex_right"])
+        ))
+    # dimensions: (batch, sensors, sequence, H, W)
+    ds["tactile"] = np.array(ds["tactile"])
+    return ds
 
 if __name__ == "__main__":
     import torch
     import pickle
     
-    from torchsummary import summary
     from torch.utils.tensorboard import SummaryWriter
 
     from tactile_insertion_rl import TactileInsertionRLNet
 
-    # read data
-    with open(f"{__file__.replace(__file__.split('/')[-1], '')}/2022-07-27-16-44-18.pkl", "rb") as f:
-        mm_data = pickle.load(f)
-    
-    # convert tactile vector to tactile matrix
-    left = reshape_mm_vector(mm_data["/tactile_left"])
-    right = reshape_mm_vector(mm_data["/tactile_right"])
-
-    # one matrix for all samples and normalize
-    data = np.concatenate([left, right], axis=0)
-    data = normalize_mm(data)
-
-    # (optional) since the outermost taxels are prone 
-    # to false positives, we can cut them away 
-    # data = remove_outer(data, B=1)
+    base_path = f"{__file__.replace(__file__.split('/')[-1], '')}"
+    ds = load_dataset(f"{base_path}/test_samples")
 
     net = TactileInsertionRLNet()
-    net(torch.randn((30, 2, 40, 16, 16)))
+    res = net(ds["tactile"])
+    print(res.shape)
+
     # summary(net, input_size=(2, 40, 16, 16))
     
     # sw = SummaryWriter()
