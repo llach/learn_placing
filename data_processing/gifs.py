f817252c3e2c38116f4d2c624222972c905f9f9f
llach <lachluca@gmail.com>
2022-08-07 15:46:16 +0200

    more gif tests

--- data_processing/gifs.py
+++ data_processing/gifs.py
@@ -1,38 +1,36 @@
 import numpy as np
 from PIL import Image
 
-from data_processing import normalize_mm, reshape_mm_vector
+from data_processing import preprocess_myrmex
 
 def upscale_repeat(frame, factor=10):
     return frame.repeat(factor, axis=1).repeat(factor, axis=2)
 
-
 import pickle
 
-# read data
-with open(f"{__file__.replace(__file__.split('/')[-1], '')}/test_samples/2022-08-05.15:25:03.pkl", "rb") as f:
+base_path = __file__.replace(__file__.split('/')[-1], '')
+with open(f"{base_path}/2022-07-27-16-44-18.pkl", "rb") as f:
     mm_data = pickle.load(f)
 
 # convert tactile vector to tactile matrix
-left = normalize_mm(reshape_mm_vector(mm_data["myrmex_left"]))
-right = normalize_mm(reshape_mm_vector(mm_data["myrmex_right"]))
+left = preprocess_myrmex(mm_data["/tactile_left"])
+right = preprocess_myrmex(mm_data["/tactile_right"])
 
 left_chan = np.zeros(list(left.shape) + [3])
 left_chan[:,:,:,2] = left
 left_chan *= 255
 left_chan = left_chan.astype(np.uint8)
+left_chan = upscale_repeat(left_chan, 35)
 
-right_chan = np.zeros(list(right.shape) + [3])
-right_chan[:,:,:,2] = right
-right_chan *= 255
-right_chan = right_chan.astype(np.uint8)
+# right_chan = np.zeros(list(right.shape) + [3])
+# right_chan[:,:,:,2] = right
+# right_chan *= 255
+# right_chan = right_chan.astype(np.uint8)
 
 imgs = [Image.fromarray(img) for img in left_chan]
-import matplotlib.pyplot as plt
+for i, img in enumerate(imgs):
+    img.save(f"{base_path}/images/{i}.png")
 
-plt.imshow(left_chan[100])
-plt.show()
-
-imgs[100].show()
+# imgs[200].show()
 # duration is the number of milliseconds between frames; this is 40 frames per second
-imgs[0].save("array.gif", save_all=True, append_images=imgs[1:], duration=100, loop=1)
+# imgs[0].save(f"{base_path}/array.gif", save_all=True, append_images=imgs[1:], duration=100, loop=1)
