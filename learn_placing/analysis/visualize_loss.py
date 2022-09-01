5164ced4d03f5b71f9fb23c505fb33c06cefc694
llach <lachluca@gmail.com>
2022-09-01 10:34:10 +0200

    fix loss

--- learn_placing/analysis/visualize_loss.py
+++ learn_placing/analysis/visualize_loss.py
@@ -18,9 +18,19 @@ def sample_random_orientation():
     return tf.quaternion_matrix(tf.quaternion_multiply(Qzt, Qyp))
 
 N=5000
-points = np.array([sample_random_orientation()[:3,:3]@[0,0,-1] for _ in range(N)])
+Rgoal = tf.Rx(-np.pi/2)
+Rs = [tf.Rx(th)@Rgoal for th in np.linspace(0, 2*np.pi, 50)]
+points = [tf.Rz(0.73)@R@[0,0,-1,1] for R in Rs]
+points = np.array([p[:3] for p in points])
 
-v = [0,-1,0]
+points = np.array([sample_random_orientation()[:3,:3]@[0,0,-1] for _ in range(N)])
+# points = np.array([p for p in points if p[1]<0])
+
+qs = np.array([vecs2quat([0,0,-1], p) for p in points])
+Rs = np.array([tf.quaternion_matrix(q) for q in qs])
+
+v = Rgoal@[0,0,-1,1]
+v = v[:3]
 
 def qloss_np(q1,q2):
     return 1-np.dot(q1,q2)**2
@@ -45,22 +55,30 @@ def qloss_np(q1,q2):
 # norm_metric = metric / np.pi
 
 # point loss
-qsample = np.repeat([vecs2quat([0,0,-1], v)], N, axis=0)
-Rsample = wrap_torch_fn(compute_rotation_matrix_from_quaternion, qsample)
-Rout = wrap_torch_fn(
-    compute_rotation_matrix_from_quaternion, 
-    np.array([vecs2quat([0,0,-1], u) for u in points])
-)
+def nikloss(Rtar, Rpred):
+    vtar = np.array([(R@[0,0,-1,1])[:3] for R in Rtar])
+    vpred = np.array([(R@[0,0,-1,1])[:3] for R in Rpred])
+    coss = np.array([np.dot(u,w) for u,w in zip(vtar, vpred)])
+    # return np.arccos(coss)
+    return 1-coss
 
-metric = wrap_torch_fn(point_loss, Rsample, Rout)
-norm_metric = metric / np.pi
+Rgoals =  np.repeat([Rgoal], Rs.shape[0], axis=0)
+
+nikloss(Rgoals, Rs)
+
+# metric = wrap_torch_fn(point_loss, Rgoals, Rs)
+# norm_metric = metric / np.pi
+
+metric = nikloss(Rgoals, Rs)
+# norm_metric = metric/np.pi
+norm_metric = metric/2
 
 cm = plt.get_cmap("copper")
 colors = cm(norm_metric)
 
-threshold = 100.0
-colors = colors[np.where(metric<threshold)]
-points = points[np.where(metric<threshold)]
+# threshold = 100.0
+# colors = colors[np.where(metric<threshold)]
+# points = points[np.where(metric<threshold)]
 
 axp = AxesPlot()
 axp.plot_points(points, c=colors)
