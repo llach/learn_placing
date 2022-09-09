4648f3dd91305fcd470c57c27b515887bf1ac512
llach <lachluca@gmail.com>
2022-09-09 18:52:21 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,5 @@ from setuptools import setup
 
 setup(name='learn_placing',
       version='0.0.1',
-      packages=["learn_placing"]
-)
-
-setup(name='execute_placing',
-      version='0.0.1',
-      packages=["execute_placing"]
+      packages=["learn_placing", "execute_placing"]
 )
