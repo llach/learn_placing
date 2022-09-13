548f8fd340a80801e86da2924cb56d58d5ee6242
llach <lachluca@gmail.com>
2022-09-13 10:55:26 +0200

    new training

--- execute_placing/nn_placing.py
+++ execute_placing/nn_placing.py
@@ -237,10 +237,11 @@ if __name__ == "__main__":
     # netname = "/home/llach/tud_datasets/batch_trainings/2022.09.08_16-42-27/OptiGripperTest/OptiGripperTest_Neps20_static_tactile_2022.09.08_16-42-28"
     # netname = "/home/llach/tud_datasets/batch_trainings/2022.09.08_17-56-42/OptiGripperTest/OptiGripperTest_Neps20_with_tap_tactile_2022.09.08_17-56-42"
 
-    netname = "/home/llach/tud_datasets/batch_trainings/w_dropout_large/2022.09.12_09-47-58/Combined1000/Combined1000_Neps40_static_tactile_2022.09.12_09-47-58"
+    # netname = "/home/llach/tud_datasets/batch_trainings/w_dropout_large/2022.09.12_09-47-58/Combined1000/Combined1000_Neps40_static_tactile_2022.09.12_09-47-58"
     # netname = "/home/llach/tud_datasets/batch_trainings/w_dropout_large/2022.09.12_09-47-58/Cuboid500/Cuboid500_Neps40_static_tactile_2022.09.12_09-49-28"
     # netname = "/home/llach/tud_datasets/batch_trainings/wo_dropout_small/2022.09.11_16-03-24_cnn_chann_5_10_fc_10_10_conv_output_5/Cuboid500/Cuboid500_Neps100_static_tactile_2022.09.11_16-06-51"
-    weights = "final"
+    netname = "/home/llach/tud_datasets/batch_trainings/ias_training_new_ds/CombinedAll/CombinedAll_Neps40_static_tactile_2022.09.13_10-37-35"
+    weights = "best"
 
     nnp = NNPlacing(netname, weights)
     r = rospy.Rate(20)
